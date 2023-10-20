9542dbabf7e55b0ee87946b13fdd4b48d5b2d370
Marcos Vinicius <85797701+strngrthnall@users.noreply.github.com>
2023-10-20 17:41:03 -0300

    Modificação de modelos e criação de Repositorio

--- src/main/java/me/dio/domain/model/Feature.java
+++ src/main/java/me/dio/domain/model/Feature.java
@@ -1,34 +1,8 @@
 package me.dio.domain.model;
 
-public class Feature{
+import jakarta.persistence.Entity;
 
-  private Long id;
-  private String icon;
-  private String description;
-
-
-  public Long getId() {
-    return this.id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getIcon() {
-    return this.icon;
-  }
-
-  public void setIcon(String icon) {
-    this.icon = icon;
-  }
-
-  public String getDescription() {
-    return this.description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
+@Entity(name = "tb_features")
+public class Feature extends BaseItem {
 
 }
--- src/main/java/me/dio/domain/model/News.java
+++ src/main/java/me/dio/domain/model/News.java
@@ -1,34 +1,8 @@
 package me.dio.domain.model;
 
-public class News{
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem{
   
-  private Long id;
-  private String icon;
-  private String description;
-
-
-  public Long getId() {
-    return this.id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getIcon() {
-    return this.icon;
-  }
-
-  public void setIcon(String icon) {
-    this.icon = icon;
-  }
-
-  public String getDescription() {
-    return this.description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
 }
